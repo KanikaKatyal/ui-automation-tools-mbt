7434132c4f1d3daa476695d0c3e858f3d6a6f779
Kanika Katyal <kanika.katyal@rakuten.com>
2022-03-10 14:34:50 +0530

    Selector_translation

--- uiautomationtools/selenium/selenium_appium_shared.py
+++ uiautomationtools/selenium/selenium_appium_shared.py
@@ -48,7 +48,7 @@ class SeleniumAppiumShared(object):
         else:
             self.execute_script('arguments[0].click();', self._active_element)
 
-    def find_element_explicitly(self, value, by='xpath', timeout=15, safe=False, many=False):
+    def find_element_explicitly(self, value, by='xpath', timeout=15, safe=False, many=False, class_conversion=True):
         """
         This wraps .find_element with an explicit timeout sleep and search.
 
@@ -58,6 +58,7 @@ class SeleniumAppiumShared(object):
             timeout (int): The search duration for an element before raising an error.
             safe (bool): Whether to catch errors on elements not found.
             many (bool): Whether to find multiple elements.
+            class_conversion(bool): It will convert Android classname to iOS calssname & vice versa.
 
         Returns:
             element (WebElement): The found element.
@@ -67,12 +68,22 @@ class SeleniumAppiumShared(object):
             by = 'id'
 
         error = None
+
+        if not class_conversion:
+          val = self.special_cases(value)
+        else:
+          val = self.path_conversion(value)
+
         timeout_ms = self.time.time() + timeout
         while self.time.time() <= timeout_ms:
             try:
                 if not many:
-                    element = self.find_element(by, value)
-                    element.get_attribute('class')
+                    element = self.find_element(by, val)
+                    if self.platform_name in 'ios':
+                      element.get_attribute('type')
+                    else:
+                        element.get_attribute('class')
+
                     if not element.is_displayed() or not element.is_enabled():
                         raise self.driver_exceptions.NoSuchElementException()
 
@@ -80,29 +91,35 @@ class SeleniumAppiumShared(object):
                     self._active_element = element
                     self._active_element.click = self.click_override
                 else:
-                    element = self.find_elements(by, value)
+                    element = self.find_elements(by, val)
                     if not element:
                         raise self.driver_exceptions.NoSuchElementException()
 
-                    if value != 'body':
+                    if val != 'body':
                         if 'native' not in self.context.lower():
-                            element = self.execute_script(
-                                "return arguments[0].filter(e => e.getAttribute('class'));", element)
+                            if self.platform_name in 'ios':
+                                 element = self.execute_script("return arguments[0].filter(e => e.getAttribute('type'));", element)
+                            else:
+                                 element = self.execute_script("return arguments[0].filter(e => e.getAttribute('class'));", element)
+
                         else:
                             # TODO - make async
-                            element = [ele for ele in element if ele.get_attribute('class')]
+                            if self.platform_name in 'ios':
+                                element = [ele for ele in element if ele.get_attribute('type')]
+                            else:
+                                element = [ele for ele in element if ele.get_attribute('class')]
 
-                self.find_element_time.append([value, timeout - (timeout_ms - self.time.time())])
+                self.find_element_time.append([val, timeout - (timeout_ms - self.time.time())])
                 return element
             except (self.driver_exceptions.NoSuchElementException,
                     self.driver_exceptions.StaleElementReferenceException) as e:
-                self.find_element_time.append([value, timeout - (timeout_ms - self.time.time())])
+                self.find_element_time.append([val, timeout - (timeout_ms - self.time.time())])
                 self.time.sleep(.15)
                 error = e
 
         if not safe:
             self.logger.error('\n')
-            error_message = f'Unable to find the {by}: {value} within {timeout} seconds.'
+            error_message = f'Unable to find the {by}: {val} within {timeout} seconds.'
             self.logger.error(f'{error_message}\n')
             raise (eval(f'self.driver_exceptions.{error.__class__.__name__}')(error_message))
 
@@ -126,12 +143,12 @@ class SeleniumAppiumShared(object):
             js_code = "return arguments[0].filter(e => e.innerText).map(e => [e.innerText, e.innerHTML]);"
             elements = self.execute_script(js_code, elements)
 
-            elements = [[[t.strip() for t in re.split(r'\n+', e[0]) if t.strip()], e[1]] for e in elements]
-            elements = [e for e in elements if len(e[0]) > 0]
+        elements = [[[t.strip() for t in re.split(r'\n+', e[0]) if t.strip()], e[1]] for e in elements]
+        elements = [e for e in elements if len(e[0]) > 0]
 
-            text = [e[0] for e in elements]
-            unique_text = list(dict.fromkeys([t2 for t1 in text for t2 in t1 if t2]))
-            if ''.join(unique_text):
+        text = [e[0] for e in elements]
+        unique_text = list(dict.fromkeys([t2 for t1 in text for t2 in t1 if t2]))
+        if ''.join(unique_text):
                 all_encompassing, unique_text = unique_subsets(text, unique_text)
 
                 # TODO - need a better way to decide on data and grab leftovers
@@ -182,3 +199,66 @@ class SeleniumAppiumShared(object):
             self.logger.info(f'Scrolled to {value}\n.')
             return element
         self.logger.info(f'Unable to js scroll due to the context to {self.context}.\n')
+
+    def xpath_extraction(self, xpath):
+        """"
+        This will split the xpath into different attributes like class, name and value.
+
+        Args:
+            xpath: The element search string.
+        """
+        pieces = {}
+        pieces['class'] = xpath.split('[')[0]
+        attributes = re.findall(r'\[.*?\]', xpath)[0][1:-1]
+        name, value = attributes.split('=')
+        pieces['name'] = name
+        pieces['new_name'] = re.sub(r'@', "", name)
+        value = value[1:-1]
+        pieces['value'] = value
+        return pieces
+
+    def path_conversion(self, value):
+        """
+        This will convert the Android element value into iOS & iOS to Android element value.
+
+        Args:
+            value: The element search string.
+        """
+        pieces = self.xpath_extraction(value)
+        if self.platform_name in 'ios':
+          mapping = {"text": "name", "content-desc": "name"}
+          names = mapping[pieces['new_name']]
+        else:
+          # Conversion of iOS selectors to Android
+          # mapping = {"name": ["text", "content-desc"]}
+          names = [pieces['new_name']]
+
+        at_value = pieces['value']
+        if type(names) is not list:
+            names = [names]
+        if '//' in value:
+             for n in names:
+              return (f'//*[@{n}="{at_value}"]')
+              break
+        else:
+            return (f'*[{n}="{at_value}"]')
+
+    def special_cases(self, xpath):
+        """
+        This will convert the special cases in which Android element value(Only classname) into iOS & iOS(Only classname) to Android element value.
+
+        Args:
+            xpath: The element search string.
+        """
+        pieces = {}
+        pieces['cla_name'] = xpath.split('[')[0]
+        if self.platform_name in 'ios':
+          mappings = {"//android.widget.EditText": "//XCUIElementTypeTextField", "//android.widget.Switch[1]": "//XCUIElementTypeButton", "//android.view.ViewGroup":'//XCUIElementTypeOther'}
+          new_value = mappings[pieces['cla_name']]
+        else:
+          # Conversion of iOS selectors to Android
+          # mappings = {"//XCUIElementTypeButton": "//android.widget.EditText", "//XCUIElementTypeTextField": "//android.widget.EditText"}
+          new_value = pieces['cla_name']
+
+
+        return new_value
